6cc400aaebdca4bf70d6e0599862d7a63606616c
RevoU-FSSE-4/module-1-gifbloks <gifbloks@gmail.com>
2024-08-23 21:12:59 +0700

    landing-page

--- frontend2/.next/static/chunks/webpack.js
+++ frontend2/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "d91d72435c339089"; }
+/******/ 		__webpack_require__.h = function() { return "82f104ad12ece70b"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
